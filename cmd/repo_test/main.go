ebde4fcd604653212fd9b25c20fd500f68501f4c
Maksim Kudryashov <mdmkudryashov@avito.ru>
2025-01-05 17:03:03 +0300

    Update test main

--- cmd/repo_test/main.go
+++ cmd/repo_test/main.go
@@ -8,7 +8,8 @@ import (
 	"gorm.io/gorm"
 
 	"my/crm-golang/internal/models/contact"
-	"my/crm-golang/internal/storage/postgres/contacts"
+	"my/crm-golang/internal/services/contacts"
+	contactsrepo "my/crm-golang/internal/storage/postgres/contacts"
 )
 
 func main() {
@@ -18,36 +19,38 @@ func main() {
 		log.Fatalf("Failed to connect to database: %v", err)
 	}
 
-	repo := contacts.New(db)
+	repo := contactsrepo.New(db)
+
+	service := contacts.NewService(repo)
 
 	newContact := &contact.Contact{
-		Name:     "Alice Smith",
-		Phone:    "1234567890",
-		Telegram: "@alicesmith",
+		Name:     "Max",
+		Phone:    "123",
+		Telegram: "@max",
 		Birthday: "1990-05-15",
 	}
 
-	if err := repo.Create(newContact); err != nil {
-		log.Fatalf("Failed to create contact: %v", err)
+	if err := service.Create(newContact); err != nil {
+		fmt.Printf("Failed to create contact: %v", err)
 	}
 	fmt.Println("Contact created successfully")
 
-	contactModel, err := repo.GetByName("Alice Smith")
+	contactModel, err := service.GetByName("Alice Smith")
 	if err != nil {
-		log.Fatalf("Failed to get contact: %v", err)
+		fmt.Printf("Failed to get contact: %v", err)
 	}
 	fmt.Printf("Contact retrieved: %+v\n", contactModel)
 
 	updateData := &contact.ContactUpdateData{
-		Phone:    "0987654321",
-		Telegram: "@alice_new",
+		//Phone:    "4566",
+		//Telegram: "@new_max_max",
 	}
-	if err := repo.Update(contactModel, updateData); err != nil {
-		log.Fatalf("Failed to update contact: %v", err)
+	if err := service.Update("Max", updateData); err != nil {
+		fmt.Printf("Failed to update contact: %v", err)
 	}
 	fmt.Println("Contact updated successfully")
 
-	if err := repo.DeleteByName("Alice Smith"); err != nil {
+	if err := service.DeleteByName("Max"); err != nil {
 		log.Fatalf("Failed to delete contact: %v", err)
 	}
 	fmt.Println("Contact deleted successfully")
