22095a4a6039bdeb449753023c5e59f94c114526
Maksim Kudryashov <mdmkudryashov@avito.ru>
2025-01-05 13:21:40 +0300

    Add repository for contacts

---
+++ internal/storage/postgres/contacts/errors.go
@@ -0,0 +1,7 @@
+package contacts
+
+import "errors"
+
+var (
+	ContactNotFoundErr = errors.New("contact not found")
+)
--- internal/storage/postgres/contacts/repository.go
+++ internal/storage/postgres/contacts/repository.go
@@ -1,31 +1,65 @@
 package contacts
 
-import "my/crm-golang/internal/models/contact"
+import (
+	"gorm.io/gorm"
+
+	"my/crm-golang/internal/models/contact"
+)
 
 type Repository struct {
 	//log internal.Log
-	//db  *sqlx.DB
+	db *gorm.DB
 }
 
-//	func New(log internal.Log, db *sqlx.DB) *Repository {
-//		return &Repository{
-//			log: log,
-//			db:  db,
-//		}
-//	}
+func New(
+	//log internal.Log,
+	db *gorm.DB,
+) *Repository {
+	return &Repository{
+		db: db,
+		//log: log,
+	}
+}
 
 func (r *Repository) GetByName(name string) (*contact.Contact, error) {
-	return nil, nil
+	var contactModel contact.Contact
+	if err := r.db.Where("name = ?", name).First(&contactModel).Error; err != nil {
+		return nil, err
+	}
+	return &contactModel, nil
 }
 
 func (r *Repository) Create(contact *contact.Contact) error {
+	if err := r.db.Create(contact).Error; err != nil {
+		return err
+	}
 	return nil
 }
 
 func (r *Repository) DeleteByName(name string) error {
+	if err := r.db.Where("name = ?", name).Delete(&contact.Contact{}).Error; err != nil {
+		return err
+	}
 	return nil
 }
 
-func (r *Repository) Update(contact *contact.Contact, contactUpdateData *contact.ContactUpdateData) error {
+func (r *Repository) Update(contactModel *contact.Contact, contactUpdateData *contact.ContactUpdateData) error {
+	updateFields := map[string]interface{}{}
+	if contactUpdateData.Name != "" {
+		updateFields["name"] = contactUpdateData.Name
+	}
+	if contactUpdateData.Phone != "" {
+		updateFields["phone"] = contactUpdateData.Phone
+	}
+	if contactUpdateData.Telegram != "" {
+		updateFields["telegram"] = contactUpdateData.Telegram
+	}
+	if contactUpdateData.Birthday != "" {
+		updateFields["birthday"] = contactUpdateData.Birthday
+	}
+
+	if err := r.db.Model(contactModel).Updates(updateFields).Error; err != nil {
+		return err
+	}
 	return nil
 }
