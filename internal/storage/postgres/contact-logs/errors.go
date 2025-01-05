b8fc95c7ffe19774b336b93608980a510fc2005b
Maksim Kudryashov <mdmkudryashov@avito.ru>
2025-01-05 14:04:37 +0300

    Add contact log repo

---
+++ internal/storage/postgres/contact-logs/errors.go
@@ -0,0 +1,7 @@
+package contact_logs
+
+import "errors"
+
+var (
+	ContactLogNotFoundErr = errors.New("contact log not found")
+)
---
+++ internal/storage/postgres/contact-logs/repository.go
@@ -0,0 +1,70 @@
+package contact_logs
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+
+	"my/crm-golang/internal/models/contact_log"
+)
+
+type Repository struct {
+	//log internal.Log
+	db *gorm.DB
+}
+
+func New(
+	//log internal.Log,
+	db *gorm.DB,
+) *Repository {
+	return &Repository{
+		db: db,
+		//log: log,
+	}
+}
+
+func (r *Repository) GetById(id int) (*contact_log.ContactLog, error) {
+	var contactLogModel contact_log.ContactLog
+	if err := r.db.Where("id = ?", id).First(&contactLogModel).Error; err != nil {
+		return nil, err
+	}
+	return &contactLogModel, nil
+}
+
+func (r *Repository) Create(contact *contact_log.ContactLog) error {
+	if err := r.db.Create(contact).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *Repository) DeleteById(id int) error {
+	if err := r.db.Where("id = ?", id).Delete(&contact_log.ContactLog{}).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *Repository) Update(contactLogModel *contact_log.ContactLog, contactUpdateData *contact_log.ContactLogUpdateData) error {
+	updateFields := r.getUpdateFields(contactUpdateData)
+	if err := r.db.Model(contactLogModel).Updates(updateFields).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *Repository) getUpdateFields(contactUpdateData *contact_log.ContactLogUpdateData) map[string]interface{} {
+	updateFields := map[string]interface{}{}
+
+	if contactUpdateData.ContactId != 0 {
+		updateFields["contact_id"] = contactUpdateData.ContactId
+	}
+	if contactUpdateData.LogMessage != "" {
+		updateFields["log_message"] = contactUpdateData.LogMessage
+	}
+	if !time.Time.IsZero(contactUpdateData.Datetime) {
+		updateFields["datetime"] = contactUpdateData.Datetime
+	}
+
+	return updateFields
+}
